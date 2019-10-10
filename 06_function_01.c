61c3e2953b900ebf34cf11717f63d64776f1040f
eunso <55428135+eunso@users.noreply.github.com>
2019-10-10 16:03:05 +0900

    06_function_01,02

--- 06_function_01.c
+++ 06_function_01.c
@@ -3,22 +3,39 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star()
+
+int sumTwo(int a, int b)
 {
-	int i;
-	for (i=0;i<10;i++)
-		printf("*");
+	//int output;
+	//output = a+b;
+	return a+b;//output;
 }
 
-int main(void)
+int square(int n)
 {
-	print_star();
-	print_star();
-	print_star();
+	return n*n;
+}
+
+int get_max(int a, int b)
+{
+	if(a>b)
+		return a;
+		
+		return b;
+}
+int main (int argc, char *argv[])
+{
+	int x, y;
+	int output;
 	
-		return 0;
+	x=2;
+	y=5;
+	
+	output = sumTwo (x,y);
+	printf("suntwo :%i\n",output);
+	printf("square : %i\n", square (x));
+	return 0;
 	}	
-	
     
 	
 	
