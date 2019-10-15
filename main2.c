1807a1df67f4de6839354ddede17deb9d1d14343
eunso <55428135+eunso@users.noreply.github.com>
2019-10-15 16:07:57 +0900

    07

--- main2.c
+++ main2.c
@@ -3,18 +3,40 @@
 #include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
-int main(void) {
-	
+int factorial(int n)
+{
+	int res=1; 
 	int i;
-	int max = 45;
+	for(i=1;i<=n;i++)
+	{	res=res*i;
 	
-	srand ((unsigned) time (NULL));
-	for (i=0;i<6;i++)
+	}
 	
-	printf("%d", i+rand()%max);
+	return res;
+}
+
+int calcCombination(int n, int r)
+{
+	int bunja, bunmo;
+	bunja= factorial(n);
+	bunmo= factorial(n-r)*factorial(r);
 	
+	return (bunja/bunmo);
+}
+
+int main(int argc, char *argv[]) {
+
+ int n, r;
+ int res;
+
+ printf ("input n and r");
+ scanf("%d %d", &n, &r);
+ 
+ res = calcCombination(n,r);
+ printf("combination result is %d\n", res);
+ 
+  
+
 	return 0;
 	}
 
